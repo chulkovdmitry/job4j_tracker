fbb3a85fd920d3df18f46abc733caed74f44f21c
chulkovdmitry <dmitry_c2000@mail.ru>
2021-04-07 16:27:42 +0300

    6. Date. Отображение даты. [#500000]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,10 +1,17 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
+    }
+
+    public LocalDateTime getDateTime() {
+        return created;
     }
 
     public Item(String name) {
