07c1941d537b616827a384ea06eb0d0694bf5a24
chulkovdmitry <dmitry_c2000@mail.ru>
2021-04-11 18:17:25 +0300

    9. toString [#500012]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,6 +4,8 @@ public class StartUI {
     public static void main(String[] args) {
         Tracker tracker = new Tracker();
         Item item = new Item();
+        item.setId(10);
+        item.setName("Bug");
         System.out.println(item);
     }
 }
