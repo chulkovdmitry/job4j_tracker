c6058087d4f7605f68bae3418251aeef84ea3a1a
chulkovdmitry <dmitry_c2000@mail.ru>
2021-04-11 11:19:36 +0300

    6. Метод замены заявки. Tracker.replace [#500008]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -23,8 +23,8 @@ public class Tracker {
         boolean rsl = false;
         int index = indexOf(id);
         if (index != -1) {
-            items[index].setId(id);
-            items[index].setName(item.getName());
+            item.setId(id);
+            items[index] = item;
             rsl = true;
         }
         return rsl;
