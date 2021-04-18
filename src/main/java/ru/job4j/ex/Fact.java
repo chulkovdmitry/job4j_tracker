ec6b5caf0e51a04e3bf9644faabd46437e2f9a0e
chulkovdmitry <dmitry_c2000@mail.ru>
2021-04-18 10:34:26 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#499954]

---
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,19 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public static void main(String[] args) {
+        int fact;
+        fact = calc(-4);
+    }
+
+    public static int calc(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Число должно быть положительным.");
+        }
+        int rsl = 1;
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+}
