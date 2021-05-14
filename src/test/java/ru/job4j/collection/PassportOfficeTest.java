6a2d246526a6698f550629f11281e94d080ee721
chulkovdmitry <dmitry_c2000@mail.ru>
2021-05-14 16:24:30 +0300

    1. Паспорт и жители [#499922]

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -14,4 +14,13 @@ public class PassportOfficeTest {
         office.add(citizen);
         assertThat(office.get(citizen.getPassport()), is(citizen));
     }
+
+    @Test
+    public void addDouble() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        office.add(new Citizen("2f44a", "Ivan Petrov"));
+        assertThat(office.get("2f44a"), is(citizen));
+    }
 }
