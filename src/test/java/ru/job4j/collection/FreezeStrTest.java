4e871c9c78135b87d364d6ef4379ae23eb44ca76
chulkovdmitry <dmitry_c2000@mail.ru>
2021-10-26 18:27:47 +0300

    1. Эксперименты с различными GC [#1590] 2

--- src/test/java/ru/job4j/collection/FreezeStrTest.java
+++ src/test/java/ru/job4j/collection/FreezeStrTest.java
@@ -6,21 +6,4 @@ import static org.hamcrest.Matchers.is;
 import org.junit.Test;
 
 public class FreezeStrTest {
-/*
-    @Test
-    public void whenEq() {
-        assertThat(FreezeStr.eq("Hello", "Hlloe"), is(true));
-    }
-
-    @Test
-    public void whenNotEq() {
-        assertThat(FreezeStr.eq("Hello", "Halle"), is(false));
-    }
-
-    @Test
-    public void whenNotMultiEq() {
-        assertThat(FreezeStr.eq("heloo", "hello"), is(false));
-    }
-
- */
 }
