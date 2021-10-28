5e234295aaa1b0be9224674e1c2680378f0e7d5a
chulkovdmitry <dmitry_c2000@mail.ru>
2021-10-28 18:28:44 +0300

    Stream

---
+++ src/main/java/ru/job4j/stream/FlatIt.java
@@ -0,0 +1,15 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class FlatIt {
+    public static List<Integer> flatten(Iterator<Iterator<Integer>> it) {
+        List<Integer> result = new ArrayList<>();
+        return result;
+    }
+}
